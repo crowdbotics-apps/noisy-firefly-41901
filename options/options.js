efb8f46df2eb4d4eacd1c9fd2691581587827458
Crowdbotics <team@crowdbotics.com>
2023-07-04 17:53:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-firefly-41901",
+  url: "https://noisy-firefly-41901.botics.co",
+  api: "https://noisy-firefly-41901.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
